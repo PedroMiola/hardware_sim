2a9955a0c90d8291df0ca31606affb8c73051a55
Pedro Poli Miola <ppmiola@inf.ufrgs.br>
2025-10-08 15:25:47 +0200

    Component Interface

---
+++ src/dataValue.hpp
@@ -0,0 +1,30 @@
+#ifndef DATAVALUE_HPP
+#define DATAVALUE_HPP
+
+#include<utility>
+
+template <typename T> class DataValue {
+private:
+    std::pair<T, bool> data;
+public:
+    DataValue() : data(std::make_pair(T(), false)) {}
+    DataValue(T value) : data(std::make_pair(value, true)) {}
+
+    T getValue() const {
+        return data.first;
+    }
+
+    bool isValid() const {
+        return data.second;
+    }
+
+    void setValue(T value) {
+        data = std::make_pair(value, true);
+    }
+
+    void unset() {
+        data.second = false;
+    }
+};
+
+#endif
---
+++ test/testDataValue.cpp
@@ -0,0 +1,25 @@
+#include "../src/dataValue.hpp"
+
+#include <iostream>
+#include <fstream>
+
+int main ()
+{
+    std::ofstream logFile("../log/output.log"); 
+    if (!logFile) {
+        std::cerr << "Error: Unable to open log file." << std::endl;
+        return 1;
+    }
+
+    DataValue<int> dv1; 
+    DataValue<int> dv2(42); 
+    logFile << "dv1 is valid (expected '0'): " << dv1.isValid() << ", value (expected '0'): " << dv1.getValue() << std::endl;
+    logFile << "dv2 is valid (expected '1'): " << dv2.isValid() << ", value (expected '42'): " << dv2.getValue() << std::endl;
+    dv1.setValue(100);
+    logFile << "After setting dv1, is valid (expected '1'): " << dv1.isValid() << ", value (expected '100'): " << dv1.getValue() << std::endl;
+    dv2.unset();
+    logFile << "After unsetting dv2, is valid (expected '0'): " << dv2.isValid() << std::endl;
+
+    logFile.close(); 
+    return 0;
+}
